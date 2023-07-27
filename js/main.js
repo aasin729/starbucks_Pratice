39bebdedb9e9dcf0890e3886cb47ef2f5381ec63
aasin729 <115336608+aasin729@users.noreply.github.com>
2023-07-27 23:20:49 +0900

    ver2.0

--- js/main.js
+++ js/main.js
@@ -42,3 +42,31 @@ window.addEventListener('scroll', _.throttle(function(){
 // _,throttle(함수, 시간)
 
 
+// visual 섹션 부분 순차적으로 이미지 나타나게하기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  //gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) *  0.7, // 0.7, 1.4, 2.1, 2.7 순으로 나타나게됨
+    opacity: 1
+  });
+});
+
+// 공지사항 수직 슬라이드 부분
+
+// new Swiper(선택자 ,옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView : 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlide : true, // 1번 슬라이드가 가운데 보이기
+  loop : true,
+  autoplay : {
+    delay: 2000
+  }
+});
